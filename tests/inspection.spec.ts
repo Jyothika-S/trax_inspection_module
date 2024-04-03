79c60ff954e940f64f5e66bae09dd386180db209
Jyothika-S <jyothi0351@gmail.com>
2024-04-03 11:35:42 +0530

    made all test as steps under one test and modified expect to expect.soft

--- tests/inspection.spec.ts
+++ tests/inspection.spec.ts
@@ -1,4 +1,4 @@
-import { Page, expect, test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 import { LoginPage } from '../pages/loginPage';
 import loginData from '../test-data/loginData.json'
 import { InspLocationPage } from '../pages/inspectionLocation';
@@ -11,53 +11,50 @@ import { InspStatisticsPage } from '../pages/inspectionStatistics';
 let inspId : string;
 let location: string;
 let venue: string;
+const baseURL = process.env.BASEURL || "";
 
-test.beforeEach('Login Test', async ({page}) => {
+test('Inspection Workflow: Login, Create, Verify Details', async ({page}) => {
     const login = new LoginPage(page);
-    await login.gotoLoginPage()
-    await login.login(loginData.username, loginData.password)
-    await login.verifyRedirection()
-});
-
-test('verify Inspection location page', async ({page}) => {
-    const homePage = new HomePage(page);
-    const inspLocation = new InspLocationPage(page);
-    await homePage.toggleSidePanel();
-    await homePage.gotoMenuPage('Inspections', 'Inspection Locations');
-    await inspLocation.verifyInspLocPage();
-    expect(inspLocation.currentURL).toBe(inspectionTestData.inspection_location)
-    expect(inspLocation.inspectionLocationsText).toContain('INSPECTION LOCATIONS')
-    expect(inspLocation.inspectionLocationsText).not.toBeNull();
-    expect(inspLocation.venue).toContainText('● Venue');
-
-    
-
-})
-
-test('Inspection', async ({page}) => {
     const homePage = new HomePage(page);
     const inspLocation = new InspLocationPage(page);
     const inspLogPage = new InspLogPage(page);
     const inspOverviewPage = new InspOverviewPage(page);
     const inspStatisticsPage = new InspStatisticsPage(page);
-//creates new inspection
-    await test.step('create new inspection', async () => {
+
+    //login to application
+    await test.step('Login to application', async () => {
+        await login.gotoLoginPage()
+        await login.login(loginData.username, loginData.password)
+        await login.verifyRedirection()
+    })
+
+    //Inspection location page verification
+    await test.step('Verify inspection location page', async () => {
         await homePage.toggleSidePanel();
         await homePage.gotoMenuPage('Inspections', 'Inspection Locations');
         await inspLocation.verifyInspLocPage();
+        expect(inspLocation.currentURL).toBe(baseURL + inspectionTestData.inspection_location)
+        expect(inspLocation.inspectionLocationsText).toContain('INSPECTION LOCATIONS')
+        expect(inspLocation.inspectionLocationsText).not.toBeNull();
+        expect(inspLocation.venue).toContainText('● Venue');
         venue = inspLocation.venueText;
         console.log("venue from spec: ",venue);
+    
+    })
+
+    //creates new inspection
+    await test.step('Create new inspection', async () => {
         await inspLocation.inspPage();
         //assertions
-        expect(inspLocation.inspIdText).toContain('Inspection #:');
-        expect(inspLocation.locationText).toContain('Location:');
-        expect(inspLocation.tableHeaderText).toContain('Element Image	Element Name	Rating	Comment With Attachment');
-        expect(inspLocation.attachmentCount).toContain('1');
-        expect(inspLocation.confirmPopupTitleText).toContain('Follow-up needed?')
-        expect(inspLocation.confirmPopupContentText).toContain('Would you like to create a follow-up alert?')
-        expect(inspLocation.completeInspYesBtn.isVisible()).toBeTruthy();
-        expect(inspLocation.completeInspNoBtn.isVisible()).toBeTruthy();
-        expect(inspLocation.currentURL).toBe(inspectionTestData.inspection_location)
+        expect.soft(inspLocation.inspIdText).toContain('Inspection #:');
+        expect.soft(inspLocation.locationText).toContain('Location:');
+        expect.soft(inspLocation.tableHeaderText).toContain('Element Image	Element Name	Rating	Comment With Attachment');
+        expect.soft(inspLocation.attachmentCount).toContain('1');
+        expect.soft(inspLocation.confirmPopupTitleText).toContain('Follow-up needed?')
+        expect.soft(inspLocation.confirmPopupContentText).toContain('Would you like to create a follow-up alert?')
+        expect.soft(inspLocation.completeInspYesBtn.isVisible()).toBeTruthy();
+        expect.soft(inspLocation.completeInspNoBtn.isVisible()).toBeTruthy();
+        expect(inspLocation.currentURL).toBe(baseURL + inspectionTestData.inspection_location)
 
         inspId = inspLocation.inspectionId;
         location = inspLocation.location;
@@ -77,29 +74,29 @@ test('Inspection', async ({page}) => {
         await inspLogPage.getLogRowContent();
 
         // Verify if log contains ID and its status - "Complete"
-        expect(inspLogPage.logRowText).toContain(inspId);
-        expect(inspLogPage.logRowText).toContain('Complete');
+        expect.soft(inspLogPage.logRowText).toContain(inspId);
+        expect.soft(inspLogPage.logRowText).toContain('Complete');
     })
 
     //verify the edit of the created inspection
-    await test.step('verify inspection edit', async() => {
+    await test.step('Verifying if the details are present on the inspection\'s edit page', async() => {
         await inspLogPage.verifyEditDetails();
         expect(inspLogPage.inspIdText).toContain(inspId);
         expect(inspLogPage.inspLocText).toContain(location);
     })
 
     //check if currently completed inspection is found in Inspection Overview
-    await test.step('verify Inspection Overview', async() => {
+    await test.step('Verifying if the details are present on the inspection\'s overview page', async() => {
         await homePage.gotoMenuPage('Inspection Overview', ' ');
         await inspOverviewPage.gotoInspOverviewPage();
         console.log("venue printed from overview: ", venue)
         await inspOverviewPage.selectVenue(venue)
         await inspOverviewPage.verifyFilteredInspection(venue)
-        expect(inspOverviewPage.inspIdText).toContain(inspId)
+        expect.soft(inspOverviewPage.inspIdText).toContain(inspId)
     })
 
     //verify inspection tab in statistics
-    await test.step('statistics ', async() => {
+    await test.step('Verifying the details in the statistics page', async() => {
         await homePage.gotoMenuPage('Statistics', ' ');
         await inspStatisticsPage.gotoInspStatisticsPage();
         await inspStatisticsPage.statisticsTabSelection('Inspection')
